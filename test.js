0b988ef127feb3bdf7d43dc83e00701046cab7c0
Ahmad Ali <aallii300300@gmail.com>
2020-03-20 11:16:36 +0000

    credentials

--- test.js
+++ test.js
@@ -1,2 +1,3 @@
 let dg = require('./index');
+console.log("you are going to see the output url")
 console.log(dg('https://drive.google.com/file/d/1PiCWHIwyQWrn4YxatPZDkB8EfegRIkIV/view?usp=sharing'));
